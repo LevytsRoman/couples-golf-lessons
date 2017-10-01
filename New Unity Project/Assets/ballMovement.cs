3a50ca203fe36c5eba152ab1b4bf17c7b2e6e11d
WALTER <WAMarroquin99@gmail.com>
2017-10-01 10:55:55 -0400

    changes to shooting ball

--- New Unity Project/Assets/ballMovement.cs
+++ New Unity Project/Assets/ballMovement.cs
@@ -10,8 +10,21 @@ public class ballMovement : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.LeftControl)){
             GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-            Instantiate(clubObj, transform.position, clubObj.rotation);
+            //Instantiate(clubObj, transform.position, clubObj.rotation);
         }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 10, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, -10, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(0,0,350);
+        } 
     }
     /*private void OnCollisionEnter(Collision collision)
     {
