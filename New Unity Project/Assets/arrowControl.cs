5d5ca582113b27dec3874b8b7a71a3da72de0791
WALTER <WAMarroquin99@gmail.com>
2017-10-01 12:08:04 -0400

    Updates to power of shot and angle of shot

---
+++ New Unity Project/Assets/arrowControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 10);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -10);
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }*/
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            GetComponent<Transform>().eulerAngles = new Vector3(90, 0, 270);
+        }
+    }
+}
--- New Unity Project/Assets/ballMovement.cs
+++ New Unity Project/Assets/ballMovement.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class ballMovement : MonoBehaviour {
     //public int power;
     public Transform clubObj;
+    public Transform arrowObj;
+    public float speed = 100;
 
     private void Update()
     {
+        // Reset for next shot
         if (Input.GetKeyDown(KeyCode.LeftControl)){
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-            //Instantiate(clubObj, transform.position, clubObj.rotation);
+            GetComponent<Rigidbody>().velocity =Vector3.zero;
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            //Instantiate(arrowObj, transform.position, arrowObj.rotation);
+            arrowObj.GetComponent<Transform>().position = transform.position;
         }
+        // Angle of shot
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.Rotate(0, 10, 0);
+            transform.Rotate(0, -10, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.Rotate(0, -10, 0);
+            transform.Rotate(0, 10, 0);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddRelativeForce(0,0,350);
-        } 
+            GetComponent<Rigidbody>().AddRelativeForce(speed,0,0);
+        }
+
+        // Power of shot
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            if (speed < 1000)
+            {
+                speed += 5;
+            } else {
+                speed = 1000;
+            }
+        } else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (speed > 5)
+            {
+                speed -= 5;
+            } else
+            {
+                speed = 1;
+            }
+        }
     }
     /*private void OnCollisionEnter(Collision collision)
     {
